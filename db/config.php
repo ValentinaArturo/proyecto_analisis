e68a6a61e860febb7cb9ce01c684874b9cefd440
XLUER5 <59056930+XLUER5@users.noreply.github.com>
2023-08-30 18:33:34 -0600

    Update config.php

--- db/config.php
+++ db/config.php
@@ -1,9 +1,18 @@
 <?php
 
-define('DB_HOSTNAME','localhost');
-define('DB_USERNAME', 'ryrarhez_admin');
-define('DB_PASSWORD', 'W1130441saa'); 
-define('DB_NAME', 'ryrarhez_invitation-expert'); 
+//Produccion
 
+// define('DB_HOSTNAME','localhost');
+// define('DB_USERNAME', 'ryrarhez_admin');
+// define('DB_PASSWORD', 'W1130441saa'); 
+// define('DB_NAME', 'ryrarhez_invitation-expert'); 
+
+
+//local
+
+define('DB_HOSTNAME','localhost:3306'); // database host name
+define('DB_USERNAME', 'root'); // database user name
+define('DB_PASSWORD', 'Luisca1704.'); // database password
+define('DB_NAME', 'Nomina'); // database name
 
 ?>
