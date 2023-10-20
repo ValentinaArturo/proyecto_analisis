6275e7b0532a6871625ed8f666f8f5baf49f748b
VicEsp97 <144083515+VicEsp97@users.noreply.github.com>
2023-10-20 00:38:50 -0600

    20231020

--- endpoint/crudbanco.php
+++ endpoint/crudbanco.php
@@ -39,7 +39,7 @@ if ($token) {
     if (Token::Verify($token, KEY)) {
         switch ($method) {
             case 'GET':
-                $query_validation = "SELECT * FROM MENU";
+                $query_validation = "SELECT * FROM BANCO";
                 $stmt_validation = $dbhost->prepare($query_validation);
                 $stmt_validation->execute();
                 $results = $stmt_validation->fetchAll(PDO::FETCH_ASSOC);
@@ -62,15 +62,11 @@ if ($token) {
                 $data = json_decode($json, true);
 
                 if (
-                    !isset($data["idModulo"]) ||
                     !isset($data["nombre"]) ||
-                    !isset($data["ordenMenu"]) ||
                     !isset($data["usuarioCreacion"]) ||
-                    $data["idModulo"] == "" ||
                     $data["nombre"] == "" ||
-                    $data["ordenMenu"] == "" ||
                     $data["usuarioCreacion"] == "" ||
-                    count($data) !== 4
+                    count($data) !== 2
                 ) {
                     echo json_encode([
                         "status" => 400,
@@ -79,66 +75,46 @@ if ($token) {
                     die();
                 }
 
-                $IdModulo = $data["idModulo"];
                 $Nombre = $data["nombre"];
-                $OrdenMenu = $data["ordenMenu"];
                 $UsuarioCreacion = $data["usuarioCreacion"];
 
-                $query_validation1 = "Select Nombre From MODULO Where IdModulo=:IdModulo";
-                $stmt_validation1 = $dbhost->prepare($query_validation1);
-                $stmt_validation1->bindParam(':IdModulo', $IdModulo);
-                $stmt_validation1->execute();
+                $query_validation = "Select Nombre From BANCO Where Nombre=:Nombre";
+                $stmt_validation = $dbhost->prepare($query_validation);
+                $stmt_validation->bindParam(':Nombre', $Nombre);
+                $stmt_validation->execute();
 
-                if ($stmt_validation1->rowCount() !== 0) {
-                    $query_validation = "Select Nombre From MENU Where Nombre=:Nombre";
-                    $stmt_validation = $dbhost->prepare($query_validation);
-                    $stmt_validation->bindParam(':Nombre', $Nombre);
-                    $stmt_validation->execute();
+                if ($stmt_validation->rowCount() == 0) {
+                    $query_post = "INSERT INTO BANCO(Nombre,FechaCreacion,UsuarioCreacion,FechaModificacion,UsuarioModificacion) 
+                    VALUES (:Nombre,:FechaHoraActual,:UsuarioCreacion,NULL,NULL)";
+                    $stmt_post = $dbhost->prepare($query_post);
+                    $stmt_post->bindParam(':Nombre', $Nombre);
+                    $stmt_post->bindParam(':FechaHoraActual', $fechaHoraActual);
+                    $stmt_post->bindParam(':UsuarioCreacion', $UsuarioCreacion);
+                    $stmt_post->execute();
 
-                    if ($stmt_validation->rowCount() == 0) {
-                        $query_post = "INSERT INTO MENU(IdModulo,Nombre,OrdenMenu,FechaCreacion,UsuarioCreacion,FechaModificacion,UsuarioModificacion) 
-                            VALUES (:IdModulo,:Nombre,:OrdenMenu,:FechaHoraActual,:UsuarioCreacion,NULL,NULL)";
-                        $stmt_post = $dbhost->prepare($query_post);
-                        $stmt_post->bindParam(':IdModulo', $IdModulo);
-                        $stmt_post->bindParam(':Nombre', $Nombre);
-                        $stmt_post->bindParam(':OrdenMenu', $OrdenMenu);
-                        $stmt_post->bindParam(':FechaHoraActual', $fechaHoraActual);
-                        $stmt_post->bindParam(':UsuarioCreacion', $UsuarioCreacion);
-                        $stmt_post->execute();
+                    if ($stmt_post->rowCount() > 0) {
+                        echo json_encode(
+                            array(
+                                "status" => 200,
+                                "msg" => "Banco agregado exitosamente"
+                            )
+                        );
 
-                        if ($stmt_post->rowCount() > 0) {
-                            echo json_encode(
-                                array(
-                                    "status" => 200,
-                                    "msg" => "Menú agregado exitosamente"
-                                )
-                            );
-
-                        } else {
-
-                            echo json_encode(
-                                array(
-                                    "status" => 401,
-                                    "msg" => "Ocurrio un error, intenta nuevamente"
-                                )
-                            );
-                        }
-                        break;
                     } else {
+
                         echo json_encode(
                             array(
                                 "status" => 401,
-                                "msg" => "¡El nombre del menú está en uso, valide!"
+                                "msg" => "Ocurrio un error, intenta nuevamente"
                             )
                         );
-                        break;
                     }
-
+                    break;
                 } else {
                     echo json_encode(
                         array(
                             "status" => 401,
-                            "msg" => "¡El Id del módulo no existe, valide!"
+                            "msg" => "¡El nombre del Banco está en uso, valide!"
                         )
                     );
                     break;
@@ -159,17 +135,13 @@ if ($token) {
                 $data = json_decode($json, true);
 
                 if (
-                    !isset($data["idMenu"]) ||
-                    !isset($data["idModulo"]) ||
+                    !isset($data["idBanco"]) ||
                     !isset($data["nombre"]) ||
-                    !isset($data["ordenMenu"]) ||
                     !isset($data["usuarioModificacion"]) ||
-                    $data["idMenu"] == "" ||
-                    $data["idModulo"] == "" ||
+                    $data["idBanco"] == "" ||
                     $data["nombre"] == "" ||
-                    $data["ordenMenu"] == "" ||
                     $data["usuarioModificacion"] == "" ||
-                    count($data) !== 5
+                    count($data) !== 3
                 ) {
                     echo json_encode([
                         "status" => 400,
@@ -178,75 +150,54 @@ if ($token) {
                     die();
                 }
 
-                $IdMenu = $data["idMenu"];
-                $IdModulo = $data["idModulo"];
+                $IdBanco = $data["idBanco"];
                 $Nombre = $data["nombre"];
-                $OrdenMenu = $data["ordenMenu"];
                 $UsuarioModificacion = $data["usuarioModificacion"];
 
-                $query_validation1 = "Select Nombre From MODULO Where IdModulo=:IdModulo";
-                $stmt_validation1 = $dbhost->prepare($query_validation1);
-                $stmt_validation1->bindParam(':IdModulo', $IdModulo);
-                $stmt_validation1->execute();
+                $query_validation = "Select Nombre From BANCO Where Nombre=:Nombre";
+                $stmt_validation = $dbhost->prepare($query_validation);
+                $stmt_validation->bindParam(':Nombre', $Nombre);
+                $stmt_validation->execute();
 
-                if ($stmt_validation1->rowCount() !== 0) {
-                    $query_validation = "Select Nombre From MENU Where Nombre=:Nombre";
-                    $stmt_validation = $dbhost->prepare($query_validation);
-                    $stmt_validation->bindParam(':Nombre', $Nombre);
-                    $stmt_validation->execute();
+                if ($stmt_validation->rowCount() == 0) {
+                    $query_post = "UPDATE BANCO SET Nombre=:Nombre,
+                    FechaModificacion=:FechaHoraActual,
+                    UsuarioModificacion=:UsuarioModificacion
+                    WHERE IdBanco=:idBanco";
+                    $stmt_post = $dbhost->prepare($query_post);
+                    $stmt_post->bindParam(':idBanco', $IdBanco);
+                    $stmt_post->bindParam(':Nombre', $Nombre);
+                    $stmt_post->bindParam(':FechaHoraActual', $fechaHoraActual);
+                    $stmt_post->bindParam(':UsuarioModificacion', $UsuarioModificacion);
+                    $stmt_post->execute();
 
-                    if ($stmt_validation->rowCount() == 0) {
-                        $query_post = "UPDATE MENU SET Nombre=:Nombre, IdModulo=:IdModulo, OrdenMenu=:OrdenMenu,
-                        FechaModificacion=:FechaHoraActual,
-                        UsuarioModificacion=:UsuarioModificacion
-                        WHERE IdMenu=:IdMenu";
-                        $stmt_post = $dbhost->prepare($query_post);
-                        $stmt_post->bindParam(':IdMenu', $IdMenu);
-                        $stmt_post->bindParam(':IdModulo', $IdModulo);
-                        $stmt_post->bindParam(':Nombre', $Nombre);
-                        $stmt_post->bindParam(':OrdenMenu', $OrdenMenu);
-                        $stmt_post->bindParam(':FechaHoraActual', $fechaHoraActual);
-                        $stmt_post->bindParam(':UsuarioModificacion', $UsuarioModificacion);
-                        $stmt_post->execute();
-
-                        if ($stmt_post->rowCount() > 0) {
-                            echo json_encode(
-                                array(
-                                    "status" => 200,
-                                    "msg" => "Menú actualizado exitosamente"
-                                )
-                            );
-
-                        } else {
-
-                            echo json_encode(
-                                array(
-                                    "status" => 401,
-                                    "msg" => "Ocurrio un error, intenta nuevamente"
-                                )
-                            );
-                        }
-                        break;
+                    if ($stmt_post->rowCount() > 0) {
+                        echo json_encode(
+                            array(
+                                "status" => 200,
+                                "msg" => "Banco actualizado exitosamente"
+                            )
+                        );
 
                     } else {
                         echo json_encode(
                             array(
                                 "status" => 401,
-                                "msg" => "¡El nombre del menú está en uso, valide!"
+                                "msg" => "Ocurrio un error, intenta nuevamente"
                             )
                         );
-                        break;
                     }
+                    break;
+                    
                 } else {
                     echo json_encode(
                         array(
                             "status" => 401,
-                            "msg" => "¡El Id del módulo no existe, valide!"
+                            "msg" => "¡El nombre del Banco está en uso, valide!"
                         )
                     );
                     break;
                 }
-
             case 'DELETE':
                 $json = file_get_contents("php://input");
                 $fechaHoraActual = date('Y-m-d H:i:s');
@@ -260,8 +211,8 @@ if ($token) {
                 }
                 $data = json_decode($json, true);
                 if (
-                    !isset($data["idMenu"]) ||
-                    $data["idMenu"] == "" ||
+                    !isset($data["idBanco"]) ||
+                    $data["idBanco"] == "" ||
                     count($data) !== 1
                 ) {
                     echo json_encode([
@@ -271,17 +222,17 @@ if ($token) {
                     die();
                 }
 
-                $IdMenu = $data["idMenu"];
-                $query_delete = "DELETE FROM MENU WHERE IdMenu=:idMenu";
+                $IdBanco = $data["idBanco"];
+                $query_delete = "DELETE FROM BANCO WHERE IdBanco=:idBanco";
                 $stmt_post = $dbhost->prepare($query_delete);
-                $stmt_post->bindParam(':idMenu', $IdMenu, PDO::PARAM_INT);
+                $stmt_post->bindParam(':idBanco', $IdBanco, PDO::PARAM_INT);
                 $stmt_post->execute();
 
                 if ($stmt_post->rowCount() > 0) {
                     echo json_encode(
                         array(
                             "status" => 200,
-                            "msg" => "Menú eliminado exitosamente"
+                            "msg" => "Banco eliminado exitosamente"
                         )
                     );
                 } else {
--- endpoint/crudtipodocumento.php
+++ endpoint/crudtipodocumento.php
@@ -39,7 +39,7 @@ if ($token) {
     if (Token::Verify($token, KEY)) {
         switch ($method) {
             case 'GET':
-                $query_validation = "SELECT * FROM MENU";
+                $query_validation = "SELECT * FROM TIPO_DOCUMENTO";
                 $stmt_validation = $dbhost->prepare($query_validation);
                 $stmt_validation->execute();
                 $results = $stmt_validation->fetchAll(PDO::FETCH_ASSOC);
@@ -62,15 +62,11 @@ if ($token) {
                 $data = json_decode($json, true);
 
                 if (
-                    !isset($data["idModulo"]) ||
                     !isset($data["nombre"]) ||
-                    !isset($data["ordenMenu"]) ||
                     !isset($data["usuarioCreacion"]) ||
-                    $data["idModulo"] == "" ||
                     $data["nombre"] == "" ||
-                    $data["ordenMenu"] == "" ||
                     $data["usuarioCreacion"] == "" ||
-                    count($data) !== 4
+                    count($data) !== 2
                 ) {
                     echo json_encode([
                         "status" => 400,
@@ -79,66 +75,46 @@ if ($token) {
                     die();
                 }
 
-                $IdModulo = $data["idModulo"];
                 $Nombre = $data["nombre"];
-                $OrdenMenu = $data["ordenMenu"];
                 $UsuarioCreacion = $data["usuarioCreacion"];
 
-                $query_validation1 = "Select Nombre From MODULO Where IdModulo=:IdModulo";
-                $stmt_validation1 = $dbhost->prepare($query_validation1);
-                $stmt_validation1->bindParam(':IdModulo', $IdModulo);
-                $stmt_validation1->execute();
+                $query_validation = "Select Nombre From TIPO_DOCUMENTO Where Nombre=:Nombre";
+                $stmt_validation = $dbhost->prepare($query_validation);
+                $stmt_validation->bindParam(':Nombre', $Nombre);
+                $stmt_validation->execute();
 
-                if ($stmt_validation1->rowCount() !== 0) {
-                    $query_validation = "Select Nombre From MENU Where Nombre=:Nombre";
-                    $stmt_validation = $dbhost->prepare($query_validation);
-                    $stmt_validation->bindParam(':Nombre', $Nombre);
-                    $stmt_validation->execute();
+                if ($stmt_validation->rowCount() == 0) {
+                    $query_post = "INSERT INTO TIPO_DOCUMENTO(Nombre,FechaCreacion,UsuarioCreacion,FechaModificacion,UsuarioModificacion) 
+                    VALUES (:Nombre,:FechaHoraActual,:UsuarioCreacion,NULL,NULL)";
+                    $stmt_post = $dbhost->prepare($query_post);
+                    $stmt_post->bindParam(':Nombre', $Nombre);
+                    $stmt_post->bindParam(':FechaHoraActual', $fechaHoraActual);
+                    $stmt_post->bindParam(':UsuarioCreacion', $UsuarioCreacion);
+                    $stmt_post->execute();
 
-                    if ($stmt_validation->rowCount() == 0) {
-                        $query_post = "INSERT INTO MENU(IdModulo,Nombre,OrdenMenu,FechaCreacion,UsuarioCreacion,FechaModificacion,UsuarioModificacion) 
-                            VALUES (:IdModulo,:Nombre,:OrdenMenu,:FechaHoraActual,:UsuarioCreacion,NULL,NULL)";
-                        $stmt_post = $dbhost->prepare($query_post);
-                        $stmt_post->bindParam(':IdModulo', $IdModulo);
-                        $stmt_post->bindParam(':Nombre', $Nombre);
-                        $stmt_post->bindParam(':OrdenMenu', $OrdenMenu);
-                        $stmt_post->bindParam(':FechaHoraActual', $fechaHoraActual);
-                        $stmt_post->bindParam(':UsuarioCreacion', $UsuarioCreacion);
-                        $stmt_post->execute();
+                    if ($stmt_post->rowCount() > 0) {
+                        echo json_encode(
+                            array(
+                                "status" => 200,
+                                "msg" => "Tipo documento agregado exitosamente"
+                            )
+                        );
 
-                        if ($stmt_post->rowCount() > 0) {
-                            echo json_encode(
-                                array(
-                                    "status" => 200,
-                                    "msg" => "Menú agregado exitosamente"
-                                )
-                            );
-
-                        } else {
-
-                            echo json_encode(
-                                array(
-                                    "status" => 401,
-                                    "msg" => "Ocurrio un error, intenta nuevamente"
-                                )
-                            );
-                        }
-                        break;
                     } else {
+
                         echo json_encode(
                             array(
                                 "status" => 401,
-                                "msg" => "¡El nombre del menú está en uso, valide!"
+                                "msg" => "Ocurrio un error, intenta nuevamente"
                             )
                         );
-                        break;
                     }
-
+                    break;
                 } else {
                     echo json_encode(
                         array(
                             "status" => 401,
-                            "msg" => "¡El Id del módulo no existe, valide!"
+                            "msg" => "¡El nombre del Tipo documento está en uso, valide!"
                         )
                     );
                     break;
@@ -159,17 +135,13 @@ if ($token) {
                 $data = json_decode($json, true);
 
                 if (
-                    !isset($data["idMenu"]) ||
-                    !isset($data["idModulo"]) ||
+                    !isset($data["idTipoDocumento"]) ||
                     !isset($data["nombre"]) ||
-                    !isset($data["ordenMenu"]) ||
                     !isset($data["usuarioModificacion"]) ||
-                    $data["idMenu"] == "" ||
-                    $data["idModulo"] == "" ||
+                    $data["idTipoDocumento"] == "" ||
                     $data["nombre"] == "" ||
-                    $data["ordenMenu"] == "" ||
                     $data["usuarioModificacion"] == "" ||
-                    count($data) !== 5
+                    count($data) !== 3
                 ) {
                     echo json_encode([
                         "status" => 400,
@@ -178,75 +150,53 @@ if ($token) {
                     die();
                 }
 
-                $IdMenu = $data["idMenu"];
-                $IdModulo = $data["idModulo"];
+                $IdTipoDocumento = $data["idTipoDocumento"];
                 $Nombre = $data["nombre"];
-                $OrdenMenu = $data["ordenMenu"];
                 $UsuarioModificacion = $data["usuarioModificacion"];
 
-                $query_validation1 = "Select Nombre From MODULO Where IdModulo=:IdModulo";
-                $stmt_validation1 = $dbhost->prepare($query_validation1);
-                $stmt_validation1->bindParam(':IdModulo', $IdModulo);
-                $stmt_validation1->execute();
+                $query_validation = "Select Nombre From TIPO_DOCUMENTO Where Nombre=:Nombre";
+                $stmt_validation = $dbhost->prepare($query_validation);
+                $stmt_validation->bindParam(':Nombre', $Nombre);
+                $stmt_validation->execute();
 
-                if ($stmt_validation1->rowCount() !== 0) {
-                    $query_validation = "Select Nombre From MENU Where Nombre=:Nombre";
-                    $stmt_validation = $dbhost->prepare($query_validation);
-                    $stmt_validation->bindParam(':Nombre', $Nombre);
-                    $stmt_validation->execute();
+                if ($stmt_validation->rowCount() == 0) {
+                    $query_post = "UPDATE TIPO_DOCUMENTO SET Nombre=:Nombre,
+                    FechaModificacion=:FechaHoraActual,
+                    UsuarioModificacion=:UsuarioModificacion
+                    WHERE IdTipoDocumento=:idTipoDocumento";
+                    $stmt_post = $dbhost->prepare($query_post);
+                    $stmt_post->bindParam(':idTipoDocumento', $IdTipoDocumento);
+                    $stmt_post->bindParam(':Nombre', $Nombre);
+                    $stmt_post->bindParam(':FechaHoraActual', $fechaHoraActual);
+                    $stmt_post->bindParam(':UsuarioModificacion', $UsuarioModificacion);
+                    $stmt_post->execute();
 
-                    if ($stmt_validation->rowCount() == 0) {
-                        $query_post = "UPDATE MENU SET Nombre=:Nombre, IdModulo=:IdModulo, OrdenMenu=:OrdenMenu,
-                        FechaModificacion=:FechaHoraActual,
-                        UsuarioModificacion=:UsuarioModificacion
-                        WHERE IdMenu=:IdMenu";
-                        $stmt_post = $dbhost->prepare($query_post);
-                        $stmt_post->bindParam(':IdMenu', $IdMenu);
-                        $stmt_post->bindParam(':IdModulo', $IdModulo);
-                        $stmt_post->bindParam(':Nombre', $Nombre);
-                        $stmt_post->bindParam(':OrdenMenu', $OrdenMenu);
-                        $stmt_post->bindParam(':FechaHoraActual', $fechaHoraActual);
-                        $stmt_post->bindParam(':UsuarioModificacion', $UsuarioModificacion);
-                        $stmt_post->execute();
-
-                        if ($stmt_post->rowCount() > 0) {
-                            echo json_encode(
-                                array(
-                                    "status" => 200,
-                                    "msg" => "Menú actualizado exitosamente"
-                                )
-                            );
-
-                        } else {
-
-                            echo json_encode(
-                                array(
-                                    "status" => 401,
-                                    "msg" => "Ocurrio un error, intenta nuevamente"
-                                )
-                            );
-                        }
-                        break;
+                    if ($stmt_post->rowCount() > 0) {
+                        echo json_encode(
+                            array(
+                                "status" => 200,
+                                "msg" => "Tipo documento actualizado exitosamente"
+                            )
+                        );
 
                     } else {
                         echo json_encode(
                             array(
                                 "status" => 401,
-                                "msg" => "¡El nombre del menú está en uso, valide!"
+                                "msg" => "Ocurrio un error, intenta nuevamente"
                             )
                         );
-                        break;
                     }
+                    break;
                 } else {
                     echo json_encode(
                         array(
                             "status" => 401,
-                            "msg" => "¡El Id del módulo no existe, valide!"
+                            "msg" => "¡El nombre del Tipo documento está en uso, valide!"
                         )
                     );
                     break;
                 }
-
             case 'DELETE':
                 $json = file_get_contents("php://input");
                 $fechaHoraActual = date('Y-m-d H:i:s');
@@ -260,8 +210,8 @@ if ($token) {
                 }
                 $data = json_decode($json, true);
                 if (
-                    !isset($data["idMenu"]) ||
-                    $data["idMenu"] == "" ||
+                    !isset($data["idTipoDocumento"]) ||
+                    $data["idTipoDocumento"] == "" ||
                     count($data) !== 1
                 ) {
                     echo json_encode([
@@ -271,17 +221,17 @@ if ($token) {
                     die();
                 }
 
-                $IdMenu = $data["idMenu"];
-                $query_delete = "DELETE FROM MENU WHERE IdMenu=:idMenu";
+                $IdTipoDocumento = $data["idTipoDocumento"];
+                $query_delete = "DELETE FROM TIPO_DOCUMENTO WHERE IdTipoDocumento=:idTipoDocumento";
                 $stmt_post = $dbhost->prepare($query_delete);
-                $stmt_post->bindParam(':idMenu', $IdMenu, PDO::PARAM_INT);
+                $stmt_post->bindParam(':idTipoDocumento', $IdTipoDocumento, PDO::PARAM_INT);
                 $stmt_post->execute();
 
                 if ($stmt_post->rowCount() > 0) {
                     echo json_encode(
                         array(
                             "status" => 200,
-                            "msg" => "Menú eliminado exitosamente"
+                            "msg" => "Tipo documento eliminado exitosamente"
                         )
                     );
                 } else {
