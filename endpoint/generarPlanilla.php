8b8ff62babf14a09976819dce8057b7487293315
XLUER5 <59056930+XLUER5@users.noreply.github.com>
2023-10-24 17:36:40 -0600

    Update generarPlanilla.php

--- endpoint/generarPlanilla.php
+++ endpoint/generarPlanilla.php
@@ -41,7 +41,90 @@ if ($token) {
     if(Token::Verify($token,KEY)){
 
 
-        switch ($method) {                
+        switch ($method) {      
+            
+            case "GET": 
+
+                $json = file_get_contents("php://input");
+                $fechaHoraActual = date('Y-m-d H:i:s');
+
+                if ($json == false || trim($json) == "") {
+                    echo json_encode([
+                        "status" => 400,
+                        "msg" => "Error en datos recibidos",
+                    ]);
+                    die();
+                }
+
+                $data = json_decode($json,true);
+
+                if (
+                    !isset($data["year"]) ||
+                    !isset($data["month"]) ||
+                    $data["year"] == "" ||
+                    $data["month"] == "" ||
+                    count($data) !== 2
+                ) {
+                    echo json_encode([
+                        "status" => 400,
+                        "msg" => "Formato de datos incorrecto",
+                    ]);
+                    die();
+                }
+
+                $year =  $data["year"];
+                $month = $data["month"];
+
+
+                $query = "SELECT * FROM PLANILLA_CABECERA WHERE Anio=:year AND Mes=:month ";
+                $stmt_post = $dbhost->prepare($query);
+                $stmt_post->bindParam(':year', $year,PDO::PARAM_INT);
+                $stmt_post->bindParam(':month', $month,PDO::PARAM_INT);
+                $stmt_post->execute();
+
+                $results_cabecera = $stmt_post->fetchAll(PDO::FETCH_ASSOC);
+                $json_cabecera = json_encode($results_cabecera);
+
+                $query_detalle = "SELECT 
+                T1.IdEmpleado,
+                T1.Anio, 
+                T1.Mes, 
+                CONCAT(T3.Nombre,' ',T3.Apellido) as 'Nombre Empleado', 
+                T1.FechaContratacion,
+                T4.Nombre as 'Puesto',
+                T5.Nombre as 'Estado Usuario',
+                T1.IngresoSueldoBase,
+                T1.IngresoBonificacionDecreto,
+                T1.IngresoOtrosIngresos,
+                T1.DescuentoIgss,
+                T1.DescuentoIsr,
+                T1.DescuentoInasistencias,
+                T1.SalarioNeto 
+                FROM PLANILLA_DETALLE T1 
+                LEFT JOIN EMPLEADO T2 ON T1.IdEmpleado = T2.IdEmpleado
+                LEFT JOIN PERSONA T3 ON T2.IdPersona = T3.IdPersona
+                LEFT JOIN PUESTO T4 ON T1.IdPuesto = T4.IdPuesto
+                LEFT JOIN STATUS_USUARIO T5 ON T1.IdStatusEmpleado = T5.IdStatusUsuario
+                WHERE T1.Anio=:year AND T1.Mes=:month";
+                $stmt_detalle = $dbhost->prepare($query_detalle);
+                $stmt_detalle->bindParam(':year', $year,PDO::PARAM_INT);
+                $stmt_detalle->bindParam(':month', $month,PDO::PARAM_INT);
+                $stmt_detalle->execute();
+    
+                $results_detalle = $stmt_detalle->fetchAll(PDO::FETCH_ASSOC);
+                $json_detalle = json_encode($results_detalle);
+
+                $combined_results = array(
+                    'cabecera' => $results_cabecera,
+                    'detalle' => $results_detalle
+                );
+
+                $json_combined = json_encode($combined_results);
+
+                echo $json_combined;
+
+                break;
+            
             case 'POST':
 
                 $json = file_get_contents("php://input");
